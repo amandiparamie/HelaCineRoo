c2c12c9c9b4c41349dad48acf6d1560f7df7e257
P. D. A. P. Kumarathunga <236312703+amandiparamie@users.noreply.github.com>
2025-12-23 11:52:21 +0530

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "EEI4369DesignProject_S22010144"
+include(":app")
+ 
